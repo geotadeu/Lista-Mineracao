51dec521a26a743e6910cb14aaa4c8a575443856
Geovane Tadeu <geovanetadeu97@hotmail.com>
2021-11-15 18:11:24 -0300

    Create 3.py

---
+++ Capítulo 5/3.py
@@ -0,0 +1,7 @@
+n1 = float(input('Informe o primeiro número:'))
+n2 = float(input('Informe o segundo número:'))
+
+print('A soma dos números é: ', n1+n2)
+print('A subtração dos números é: ', n1-n2)
+print('A multiplicação dos números é: ', n1*n2)
+print('A divisão dos números é: ', n1/n2)
